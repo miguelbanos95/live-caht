515f28ae74985248bcd5a5473306ee2c64fae1c2
Miguel Baños <info.miguelbanos@gmail.com>
2022-09-15 21:14:24 +0200

    some back end progress, add errors in routes etc

--- public/src/App.js
+++ public/src/App.js
@@ -1,8 +1,21 @@
+import { Routes, Route } from "react-router-dom"; 
+import Register from "./views/Register/Register"
+import Login from "./views/Login/Login"
+import Chat from "./views/Chat/Chat"
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>holo</h1>
+      <Routes>
+        <Route path="/" element={<Chat/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+
+      </Routes>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,25 +1,71 @@
 require('dotenv').config();
 
-
 const express = require('express')
 const cors = require('cors');
+const createError = require('http-errors')
 const mongoose = require('mongoose');
+const logger = require('morgan');
+const jwt = require('jsonwebtoken')
 
 require('./config/db.config');
 
 
 const app = express();
 
+/* Middlewares */
 
-app.use(cors()); 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
 
+/* Routes */
+
+const routes = require('./config/routes')
+app.use('/api', routes)
+
+/* Handle errors */
+
+app.use((req, res, next) => {
+    next(createError(404, 'Page not found'))
+})
+
+app.use((error, req, res, next) => {
+    if (error instanceof mongoose.Error.ValidationError) {
+        error = createError(400, error);
+        console.log(error)
+    } else if (error instanceof mongoose.Error.CastError) {
+        error = createError(404, "Resource not found");
+    } else if (error.message && error.message.includes("E11000")) {
+        error = createError(400, "Already exists");
+    } else if (error instanceof jwt.JsonWebTokenError) {
+        error = createError(401, error);
+    } else if (!error.status) {
+        error = createError(500, error);
+    }
+    if (error.status >= 500) {
+        console.error(error);
+    }
+
+    const data = {};
+    data.message = error.message;
+    data.errors = error.errors
+        ? Object.keys(error.errors).reduce(
+            (errors, key) => ({
+                ...errors,
+                [key]: error.errors[key].message || error.errors[key],
+            }),
+            {}
+        )
+        : undefined;
+
+    res.status(error.status).json(data);
+})
 
 /**
  * Port we are going to use
  */
- const port = Number(process.env.PORT || 3000);
- app.listen(port, async() => {
-     console.log(`Ey! Your port ${port} is now available!`);
- });
- 
+
+const port = Number(process.env.PORT || 3000);
+app.listen(port, async () => {
+    console.log(`Ey! Your port ${port} is now available!`);
+});
